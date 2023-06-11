22dc70d52fd0def55f326de94997cfe88ebca01a
Yokomi <38473135+Yokomide@users.noreply.github.com>
2023-06-11 16:40:53 +0300

    Update
    chg: code style

--- Assets/Scripts/ResourceContainer3D.cs
+++ Assets/Scripts/ResourceContainer3D.cs
@@ -1,44 +1,39 @@
 using DG.Tweening;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class ResourceContainer3D : MonoBehaviour
 {
     public List<GameObject> resourceModels = new List<GameObject>();
-    public Transform ContainerHolder;
-    public Vector3 TargetPosition;
-    private Sequence _addSequence;
-    [SerializeField] float _resourceFlyDuration;
-    [SerializeField] float _resourceFollowDuration;
 
-    [SerializeField] AnimationCurve _resourceFlyCurve; 
+    [SerializeField] private Transform _containerHolder;
+    [SerializeField] private AnimationCurve _resourceFlyCurve;
+
+    [SerializeField] float _resourceFlyDuration;
+
     public void AddResource(GameObject resource)
     {
         resourceModels.Add(resource);
-        Tweener tweener = resource.transform.DOMove(ContainerHolder.position + new Vector3(0, 0.1f * resourceModels.IndexOf(resource), 0), _resourceFlyDuration).OnComplete(() => resource.transform.parent = ContainerHolder.transform);
+
+        Tweener tweener = resource.transform.DOMove(_containerHolder.position + new Vector3(0, 0.1f * resourceModels.IndexOf(resource), 0), _resourceFlyDuration).OnComplete(() => resource.transform.parent = _containerHolder.transform);
         tweener.SetUpdate(UpdateType.Fixed);
         tweener.OnUpdate(() =>
         {
-            if (Vector3.Distance(resource.transform.position, (ContainerHolder.position + new Vector3(0, 0.1f * resourceModels.IndexOf(resource)))) > 0.2f)
+            if (Vector3.Distance(resource.transform.position, (_containerHolder.position + new Vector3(0, 0.1f * resourceModels.IndexOf(resource)))) > 0.2f)
             {
                 Debug.Log(resourceModels.IndexOf(resource));
-                tweener.ChangeEndValue(ContainerHolder.position + new Vector3(0, 0.1f * resourceModels.IndexOf(resource), 0), _resourceFollowDuration, true);
+                tweener.ChangeEndValue(_containerHolder.position + new Vector3(0, 0.1f * resourceModels.IndexOf(resource), 0), true);
             }
             else
             {
-                resource.transform.parent = ContainerHolder.transform;
-                resource.transform.position = ContainerHolder.position + new Vector3(0, 0.1f * resourceModels.IndexOf(resource), 0);
+                resource.transform.parent = _containerHolder.transform;
+                resource.transform.position = _containerHolder.position + new Vector3(0, 0.1f * resourceModels.IndexOf(resource), 0);
                 tweener.Kill();
                 Debug.Log("Сработало");
             }
         });
             resource.transform.DORotate(new Vector3(-90, 0, 0), 1f);
         }
-    private void FixedUpdate()
-    {
-        TargetPosition = ContainerHolder.position;
-    }
     public void RemoveResource()
     {
 
