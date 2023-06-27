df2c133695ee6491245544601a10796f3c7254b0
Maksim Demian <80910641+DeadSunset@users.noreply.github.com>
2023-06-27 18:14:55 +0300

    Много ассетов

--- Assets/Scripts/TweenAnimationContoller.cs
+++ Assets/Scripts/TweenAnimationContoller.cs
@@ -12,34 +12,21 @@ public class TweenAnimationContoller : MonoBehaviour
     [TableList(ShowIndexLabels = true)]
     public List<AnimationSettings> _settings = new List<AnimationSettings>();
 
-    public List<Tween> _storedAnimation = new List<Tween>();
-    public List<GameObject> _animationTargets = new List<GameObject>();
-
-
     [Serializable]
     public class AnimationSettings
     {
-        [EnumPaging]
-        public AnimationType animationType;
+        public AnimationType _animationType;
+        public Transform _targetPosition;
+        public Vector3 _scaleTarget;
 
         public float _duration;
-        [ShowIf("@animationType == AnimationType.Move || animationType == AnimationType.Jump")]
-        public Transform _targetPosition;
-        [ShowIf("@animationType == AnimationType.ScaleFromZero || animationType == AnimationType.ScaleToZero || animationType == AnimationType.ScaleFromCurrent")]
-        public Vector3 _scaleTarget;
-        [ShowIf("animationType", AnimationType.Jump)]
         public float _jumpForce;
-        [ShowIf("animationType", AnimationType.Jump)]
         public int _numJumps;
-        //public bool _loop;
-
-        public bool _storeAnimation;
-
 
     }
     public void StartAnimation(GameObject animationTarget, int index)
     {
-        switch (_settings[index].animationType)
+        switch (_settings[index]._animationType)
         {
             case AnimationType.ScaleFromZero:
                 ScaleFromZero(animationTarget, index);
@@ -90,120 +77,49 @@ public class TweenAnimationContoller : MonoBehaviour
 
         }
     }
-
-    public void RewindAnimation(int index)
-    {
-        _storedAnimation[index].SmoothRewind();
-
-        _storedAnimation[index].OnComplete(() => _animationTargets[index].SetActive(false));
-    }
     private void ScaleToZero(GameObject animationTarget, int index)
     {
-        var animation = animationTarget.transform.DOScale(Vector3.zero, _settings[index]._duration)
-             .SetAutoKill(false);
-
-        animation.OnComplete(() =>
-        {
-            if (_callAnimationEndEvent)
-                OnAnimationEnd?.Invoke();
-            if (!_settings[index]._storeAnimation)
+        animationTarget.transform.DOScale(Vector3.zero, _settings[index]._duration)
+            .OnComplete(() =>
             {
-                animation.Kill();
-            }
-        });
-        if (_settings[index]._storeAnimation)
-        {
-            _storedAnimation.Add(animation);
-            _animationTargets.Add(animationTarget);
-        }
+                animationTarget.SetActive(false);
+                if(_callAnimationEndEvent)
+                OnAnimationEnd?.Invoke();
+            });
 
     }
     private void ScaleFromZero(GameObject animationTarget, int index)
     {
         animationTarget.transform.localScale = Vector3.zero;
-        var animation = animationTarget.transform.DOScale(_settings[index]._scaleTarget, _settings[index]._duration)
-              .SetAutoKill(false);
-
-        animation.OnComplete(() =>
+        animationTarget.transform.DOScale(_settings[index]._scaleTarget, _settings[index]._duration).OnComplete(() =>
         {
             if (_callAnimationEndEvent)
                 OnAnimationEnd?.Invoke();
-            if (!_settings[index]._storeAnimation)
-            {
-                animation.Kill();
-            }
         });
-        if (_settings[index]._storeAnimation)
-        {
-            _storedAnimation.Add(animation);
-            _animationTargets.Add(animationTarget);
-        }
     }
     private void ScaleFromCurrent(GameObject animationTarget, int index)
     {
-        var animation = animationTarget.transform.DOScale(_settings[index]._scaleTarget, _settings[index]._duration)
-             .SetAutoKill(false);
-
-        animation.OnComplete(() =>
+        animationTarget.transform.DOScale(_settings[index]._scaleTarget, _settings[index]._duration).OnComplete(() =>
         {
             if (_callAnimationEndEvent)
                 OnAnimationEnd?.Invoke();
-            if (!_settings[index]._storeAnimation)
-            {
-                animation.Kill();
-            }
         });
-        if (_settings[index]._storeAnimation)
-        {
-            _storedAnimation.Add(animation);
-            _animationTargets.Add(animationTarget);
-        }
     }
     private void Move(GameObject animationTarget, int index)
     {
-        var animation = animationTarget.transform.DOMove(_settings[index]._targetPosition.position, _settings[index]._duration)
-             .SetAutoKill(false);
-
-        animation.OnComplete(() =>
+        animationTarget.transform.DOMove(_settings[index]._targetPosition.position, _settings[index]._duration).OnComplete(() =>
         {
             if (_callAnimationEndEvent)
                 OnAnimationEnd?.Invoke();
-            if (!_settings[index]._storeAnimation)
-            {
-                animation.Kill();
-            }
         });
-        if (_settings[index]._storeAnimation)
-        {
-            _storedAnimation.Add(animation);
-            _animationTargets.Add(animationTarget);
-        }
     }
     private void Jump(GameObject animationTarget, int index)
     {
-        var animation = animationTarget.transform.DOJump(_settings[index]._targetPosition.position, _settings[index]._jumpForce, _settings[index]._numJumps, _settings[index]._duration)
-             .SetAutoKill(false);
-
-        animation.OnComplete(() =>
+        animationTarget.transform.DOJump(_settings[index]._targetPosition.position , _settings[index]._jumpForce, _settings[index]._numJumps, _settings[index]._duration).OnComplete(() =>
         {
             if (_callAnimationEndEvent)
                 OnAnimationEnd?.Invoke();
-            if (!_settings[index]._storeAnimation)
-            {
-                animation.Kill();
-            }
         });
-        if (_settings[index]._storeAnimation)
-        {
-            _storedAnimation.Add(animation);
-            _animationTargets.Add(animationTarget);
-        }
-    }
-
-    public void ClearStored()
-    {
-        _storedAnimation.Clear();
-        _animationTargets.Clear();
     }
 
 }
